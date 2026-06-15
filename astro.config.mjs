db76787f32a0f4b85d5e24fa3b70956a833374b8
Wade Randel <waderandel6@gmail.com>
2026-06-14 19:16:43 -0600

    Added configuration for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://wade-randel.github.io',
+  base: '/MotiventCodingProject',
   integrations: [react()]
 });
